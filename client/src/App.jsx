191f74d596469860f4387af9c157f85f56ef97cd
Yaswanth <yaswanth@Yaswanths-MacBook-Air-2.local>
2025-09-13 18:13:39 +0530

    client setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='bg-black text-white'>Welcome bro!</div>
+    </>
+  )
+}
+
+export default App
